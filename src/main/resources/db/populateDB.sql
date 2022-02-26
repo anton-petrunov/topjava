0b9468c2ecf2ec171650549c64553909ec75b2f5
Anton Petrunov <a.n.tone@mail.ru>
2022-02-26 22:49:47 +0300

    Initialise and populate DB table meals. Implements JdbcMealRepository. Check MealServlet

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -10,3 +11,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2022-02-22T00:00:00', 'Dinner in McD', 4000),
+       (100000, '2022-02-23T17:13:03', 'Christmas post', 40);
