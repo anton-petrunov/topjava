cf57985d228b89ce02330a481869981daa5de7bd
Anton Petrunov <a.n.tone@mail.ru>
2022-03-12 15:15:08 +0300

    Add AbstractServiceTest. Implement all tests by inheritance

---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+@ActiveProfiles("datajpa")
+public class DataJpaUserServiceTest extends AbstractUserServiceTest {
+}
