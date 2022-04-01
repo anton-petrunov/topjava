95e00927affde9daedb4d0ca24bbc32c144e1af5
Anton Petrunov <a.n.tone@mail.ru>
2022-04-01 15:16:15 +0300

    Add webjars, jQuery. Fix ProfileRestController.getWithMeals

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -34,8 +34,8 @@ public class ProfileRestController extends AbstractUserController {
         return "Русский текст";
     }
 
-    @GetMapping("/{id}/with-meals")
-    public User getWithMeals(int id) {
-        return super.getWithMeals(id);
+    @GetMapping("/with-meals")
+    public User getWithMeals() {
+        return super.getWithMeals(authUserId());
     }
 }
